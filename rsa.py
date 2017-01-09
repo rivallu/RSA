ff6b04ff4f9c02d0d0e70338884ca704de633525
Lucas Rival <lucas.rival@gmail.com>
2017-01-09 10:47:28 +0100

    added prototype of function

--- rsa.py
+++ rsa.py
@@ -15,3 +15,32 @@
 # @param param : le paramètre                                                       #
 # @return return : le paramètre de retour		                                    #
 #####################################################################################
+
+def IsFirst(intA):
+
+#####################################################################################
+# Description de la fonction									                    #
+# @param param : le paramètre                                                       #
+# @return return : le paramètre de retour		                                    #
+#####################################################################################
+
+def SontPremierEntreEux(intA, intB):
+
+
+#####################################################################################
+# Description de la fonction									                    #
+# @param param : le paramètre                                                       #
+# @return return : le paramètre de retour		                                    #
+#####################################################################################
+
+def TrouvePremier(max):
+
+
+
+#####################################################################################
+# Description de la fonction									                    #
+# @param param : le paramètre                                                       #
+# @return return : le paramètre de retour		                                    #
+#####################################################################################
+
+def CalculInverse(inta, intMod)
