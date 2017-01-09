f62a2e2d7b6244a4e45f2a49502ced8884777de2
Lucas Rival <lucas.rival@gmail.com>
2017-01-09 11:14:23 +0100

    Added function IsFirst and some test

--- rsa.py
+++ rsa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.5
 #-*-coding:UTF-8-*-
 
-
+import math
 
 #####################################################################################
 #                                       Variable Globale                            #
@@ -11,30 +11,19 @@
 
 
 #####################################################################################
-# Description de la fonction									                    #
-# @param param : le paramètre                                                       #
-# @return return : le paramètre de retour		                                    #
+# Test si un nombre est premier     							                    #
+# @param intA : un nombre                                                           #
+# @return result : Retourne vrai ou faux		                                    #
 #####################################################################################
 
 def IsFirst(intA):
-
-#####################################################################################
-# Description de la fonction									                    #
-# @param param : le paramètre                                                       #
-# @return return : le paramètre de retour		                                    #
-#####################################################################################
-
-def SontPremierEntreEux(intA, intB):
-
-
-#####################################################################################
-# Description de la fonction									                    #
-# @param param : le paramètre                                                       #
-# @return return : le paramètre de retour		                                    #
-#####################################################################################
-
-def TrouvePremier(max):
-
+    result=True
+    racine=int(math.sqrt(intA))
+    for i in range(1,racine):
+        reste=intA%i
+        if reste==0 and i!=1:
+            result=False
+    return result
 
 
 #####################################################################################
@@ -43,4 +32,30 @@ def TrouvePremier(max):
 # @return return : le paramètre de retour		                                    #
 #####################################################################################
 
-def CalculInverse(inta, intMod)
+# def SontPremierEntreEux(intA, intB):
+
+
+#####################################################################################
+# Description de la fonction									                    #
+# @param param : le paramètre                                                       #
+# @return return : le paramètre de retour		                                    #
+#####################################################################################
+
+# def TrouvePremier(max):
+
+
+
+#####################################################################################
+# Description de la fonction									                    #
+# @param param : le paramètre                                                       #
+# @return return : le paramètre de retour		                                    #
+#####################################################################################
+
+# def CalculInverse(inta, intMod):
+
+
+if __name__ == '__main__':
+    assert(IsFirst(40)==False)
+    assert(IsFirst(7)==True)
+    assert(IsFirst(517)==False)
+    assert(IsFirst(173)==True)
